542bbe45ecbf0eb8d09066e565261e5dc38831f8
viveksingh6876 <viveksingh6876@gmail.com>
2023-12-09 14:49:20 +0530

    class and tag name

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var listItem = document.getElementsByClassName("list-group-item");
+// var listItem = document.getElementsByClassName("list-group-item");
 
 
-listItem[2].style.backgroundColor = "green";
+// listItem[2].style.backgroundColor = "green";
 
 
-for(var i=0; i < listItem.length; i++ ){
-    listItem[i].style.fontWeight = "bold";
-}
+// for(var i=0; i < listItem.length; i++ ){
+//     listItem[i].style.fontWeight = "bold";
+// }
 
+
+var li = document.getElementsByTagName("li")
+
+li[0].style.color = "red"
+li[2].style.fontWeight = "bold"
+
+for(var i=0; i < li.length; i++){
+    li[i].style.backgroundColor = "yellow"
+} 
+
+console.log(li.length)
