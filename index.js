94ebd80bc918d384c733fb52d93534ea045811ae
viveksingh6876 <viveksingh6876@gmail.com>
2023-12-10 10:56:02 +0530

    Queryselector commit

--- index.js
+++ index.js
@@ -9,13 +9,32 @@
 // }
 
 
-var li = document.getElementsByTagName("li")
+// var li = document.getElementsByTagName("li")
 
-li[0].style.color = "red"
-li[2].style.fontWeight = "bold"
+// li[0].style.color = "red"
+// li[2].style.fontWeight = "bold"
 
-for(var i=0; i < li.length; i++){
-    li[i].style.backgroundColor = "yellow"
-} 
+// for(var i=0; i < li.length; i++){
+//     li[i].style.backgroundColor = "yellow"
+// } 
 
-console.log(li.length)
+// console.log(li.length)
+
+//QUERYSELECTOR
+
+// var secondListItem = document.querySelector(".list-group-item:nth-child(1)");
+// secondListItem.style.backgroundColor = "green"
+
+// var thirdListItem = document.querySelector(".list-group-item:nth-child(2)");
+// thirdListItem.style.display = "none"
+
+////QUERYSELECTORALL
+
+var oddListItem = document.querySelectorAll("li:nth-child(odd)")
+
+for(var i = 0; i<oddListItem.length; i++ ){
+    oddListItem[i].style.backgroundColor = "green"
+}
+
+var secondListItem = document.querySelectorAll("li")
+secondListItem[1].style.color = "blue"
