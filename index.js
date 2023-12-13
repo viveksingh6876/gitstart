0c0c39dfcf09d801427c2139cb158e7fb370ddb1
viveksingh6876 <viveksingh6876@gmail.com>
2023-12-13 21:41:24 +0530

    submit and click event listener with add and delete functionality

--- index.js
+++ index.js
@@ -1,86 +1,38 @@
-// var listItem = document.getElementsByClassName("list-group-item");
+var form = document.getElementById("addForm");
+var listItem = document.getElementById("items");
+
+form.addEventListener("submit", additem);
+
+listItem.addEventListener("click", removeItem);
+
+function additem(e) {
+    e.preventDefault()
+
+    var inputValue = document.getElementById("inputtextvalue").value;
+    
+    var newListItem = document.createElement("li");
+    newListItem.className = "list-group-item"
+
+    newListItem.appendChild(document.createTextNode(inputValue))
+
+    var neweditbtn = document.createElement("button")
+    neweditbtn.appendChild(document.createTextNode("Edit"))
+    neweditbtn.className = "btn btn-danger btn-sm float-right delete"
+
+    var newbtn = document.createElement("button")
+    newbtn.className = "btn btn-danger btn-sm float-right delete"
+    newbtn.appendChild(document.createTextNode("X"))
+    newListItem.appendChild(newbtn)
+    newListItem.appendChild(neweditbtn)
+    listItem.appendChild(newListItem)
+}
 
 
-// listItem[2].style.backgroundColor = "green";
-
-
-// for(var i=0; i < listItem.length; i++ ){
-//     listItem[i].style.fontWeight = "bold";
-// }
-
-
-// var li = document.getElementsByTagName("li")
-
-// li[0].style.color = "red"
-// li[2].style.fontWeight = "bold"
-
-// for(var i=0; i < li.length; i++){
-//     li[i].style.backgroundColor = "yellow"
-// } 
-
-// console.log(li.length)
-
-//QUERYSELECTOR
-
-// var secondListItem = document.querySelector(".list-group-item:nth-child(1)");
-// secondListItem.style.backgroundColor = "green"
-
-// var thirdListItem = document.querySelector(".list-group-item:nth-child(2)");
-// thirdListItem.style.display = "none"
-
-////QUERYSELECTORALL
-
-// var oddListItem = document.querySelectorAll("li:nth-child(odd)")
-
-// for(var i = 0; i<oddListItem.length; i++ ){
-//     oddListItem[i].style.backgroundColor = "green"
-// }
-
-// var secondListItem = document.querySelectorAll("li")
-// secondListItem[1].style.color = "blue"
-
-//parentElement
-var listItem = document.querySelector('#items')
-
-// console.log(listItem.parentElement)
-// console.log(listItem.parentElement.parentElement.parentElement)
-
-//lastElementChild
-//console.log(listItem.lastElementChild)
-
-//lastChild
-//console.log(listItem.lastChild)
-
-//firstElementChild
-//console.log(listItem.firstElementChild.)
-
-//firstChild
-//console.log(listItem.firstChild)
-
-//nextSibling
-//console.log(listItem.nextSibling)
-
-//nextElementSibling
-//console.log(listItem.nextElementSibling)
-
-//previousSibling
-//console.log(listItem.previousSibling)
-
-//previousSibling
-//console.log(listItem.previousElementSibling)
-
-//createElement
-
-var newdiv = document.createElement("div");
-newdiv.className="hello";
-newdiv.id = "hello1";
-newdiv.setAttribute('title', 'hello div')
-
-var textnode = document.createTextNode("New Div Element")
-
-newdiv.appendChild(textnode)
-
-var container = document.querySelector("header .container")
-var h1 = document.querySelector("header h1")
-
-container.insertBefore(newdiv, h1)
+function removeItem(e){
+    if(e.target.classList.contains("delete")){
+        if(confirm("Are you sure?")){
+            var li = e.target.parentElement
+            listItem.removeChild(li)
+        }
+    }
+}
